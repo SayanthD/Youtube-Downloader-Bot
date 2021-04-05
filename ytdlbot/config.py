07a9dcd9c2baff0112ccbe8826f4de86d66ad448
SayanthD <13906889+SayanthD@users.noreply.github.com>
2021-04-05 16:25:23 +0530

    config.py: Minor fix with DOWNLOAD_DIR

--- ytdlbot/config.py
+++ ytdlbot/config.py
@@ -5,7 +5,7 @@ class Config:
     APP_ID = int(os.environ.get("APP_ID", 0))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
+    DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "downloads")
     TIMEOUT = int(os.environ.get("TIMEOUT", 0))  # time in minute
     MAX_SIZE = int(os.environ.get("MAX_SIZE", 1900000000))
     CUSTOM_THUMB = os.environ.get("CUSTOM_THUMB", None)
